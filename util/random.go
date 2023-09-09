487cf245800d57cd99f8c6d11ca499e3b73fbdba
Vivek Jha <vivekjha3080@gmail.com>
2023-09-09 13:22:34 +0530

    error
    Signed-off-by: Vivek Jha <vivekjha3080@gmail.com>

--- util/random.go
+++ util/random.go
@@ -12,7 +12,7 @@ func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 func RandomInt(min, max int64) int64 {
-	return min + rand.Int63n(min-max+1)
+	return min + rand.Int63n(max-min+1)
 }
 
 func RandomString(n int) string {
